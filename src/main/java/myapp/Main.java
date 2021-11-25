b326852b6e297a67723df5b02afdd3fc87ec817e
Dmitriy L <DandLitvinov@sberbank.ru>
2021-11-25 15:04:36 +0300

    add feign

--- src/main/java/myapp/Main.java
+++ src/main/java/myapp/Main.java
@@ -1,9 +1,14 @@
 package myapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+
+@EnableFeignClients
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
--- src/main/java/myapp/controller/MyController.java
+++ src/main/java/myapp/controller/MyController.java
@@ -2,16 +2,47 @@ package myapp.controller;
 
 import io.swagger.annotations.*;
 import myapp.model.MyObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @Api("@Api аннотация MyController")
+@RequestMapping("/mycontr")
 public class MyController {
 
+    //@Autowired
+    //FeignService feignService;
+
     List<MyObject> objectList = new ArrayList<>();
+
+    @GetMapping()
+    public String myStrings() {
+        return "/obj /obj/new /obj/new2 /test";
+    }
+
+    @ApiOperation("Взаимодействие с другим апи через FeignClient")
+    @GetMapping("/feign")
+    public String feign() {
+        RestTemplate template = new RestTemplate();
+        String feignResourceUrl = "http://127.0.0.1:8777/feign";
+        String result = template.getForObject(feignResourceUrl,String.class);
+        return result;
+    }
+
+    @ApiOperation("Обычное взаимодействие с другим апи")
+    @GetMapping("/notFeign")
+    public String notFeign() {
+        RestTemplate template = new RestTemplate();
+        String feignResourceUrl = "http://127.0.0.1:8777/feign";
+        String result = template.getForObject(feignResourceUrl,String.class);
+        return result;
+    }
 
     @ApiOperation("Получение всех объектов")
     @RequestMapping(method = RequestMethod.GET, value = "/obj")
@@ -24,6 +55,7 @@ public class MyController {
             @ApiResponse(code = 504, message = "System over 504"),
             @ApiResponse(code = 505, message = "System over 505")
     })
+
     @PostMapping(value = "/obj/new")
     public MyObject addObject(@ApiParam(value = "Give name to object", required = true) @RequestParam String name) {
         int id = objectList.size() + 1;
@@ -32,4 +64,17 @@ public class MyController {
         return myobject;
     }
 
+    @PostMapping(value = "/obj/new2")
+    public ResponseEntity<MyObject> addObjectEntity(@RequestBody MyObject object) {
+        int id = objectList.size() + 1;
+        MyObject myobject = MyObject.builder().id(id).name(object.getName()).build();
+        objectList.add(myobject);
+        return new ResponseEntity<>(myobject, HttpStatus.OK);
+    }
+
+    @ApiOperation("Получение всех объектов")
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public String testObject() {
+        return "tesstString";
+    }
 }
